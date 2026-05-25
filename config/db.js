11331816441a09e058269a73e8b1c17bc59185b0
Uldyssian <mickael.brouttier@gmail.com>
2026-05-25 15:29:18 +0200

    On est là

--- config/db.js
+++ config/db.js
@@ -1,17 +1,15 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config();
+const fs = require('fs');
 
-const pool = mysql.createPool({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: parseInt(process.env.DB_PORT || '3306', 10),
-  waitForConnections: true,
-  connectionLimit: 10,
+  port: process.env.DB_PORT,
   ssl: {
-    rejectUnauthorized: false
+    ca: fs.readFileSync('./ca.pem') 
   }
 });
 
-module.exports = pool;
+module.exports = db;
