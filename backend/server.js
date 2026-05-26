36475bd0d0fecee949c9fba84353657e5cb3fca1
Mickaël BROUTTIER <mickael.brouttier@gmail.com>
2026-05-26 16:31:09 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,7 @@ app.use(express.json());
 db.query("SET time_zone = '+02:00'");
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp-relay.brevo.co', 
-  port: 587,
-  secure: false, 
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
